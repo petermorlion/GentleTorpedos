ac4257fb4ff55243e4a9d2af410ef05fda45a069
Peter Morlion <peter.morlion@live.be>
2016-06-07 19:58:42 +0200

    gulp build basic page

---
+++ gulpfile.js
@@ -0,0 +1,27 @@
+'use strict';
+
+var gulp = require('gulp');
+var sass = require('gulp-sass');
+
+var paths = {
+  scss: './src/**/*.scss',
+  html: './src/**/*.html',
+  dist: './dist'
+};
+
+gulp.task('sass', function () {
+  return gulp.src(paths.scss)
+    .pipe(sass().on('error', sass.logError))
+    .pipe(gulp.dest(paths.dist));
+});
+
+gulp.task('sass:watch', function () {
+  gulp.watch(paths.scss, ['sass']);
+});
+
+gulp.task('html', function() {
+  return gulp.src(paths.html)
+    .pipe(gulp.dest(paths.dist))
+});
+
+gulp.task('build', ['sass', 'html'])
